b5755cf0cb22e95f720f55cde31525a601fb78ca
AaronGuitar <1152098646@qq.com>
2018-06-04 14:16:34 +0800

    装饰器实现单例模式的四种方式

--- Base/装饰器实现单例模式四种方式.py
+++ Base/装饰器实现单例模式四种方式.py
@@ -73,7 +73,7 @@ class Singleton(type):
 
     def __call__(self, *args, **kwargs):
         if self not in self._instance:
-            self._instance[self] = super(Singleton, self).__call__(*args,**kwargs)
+            self._instance[self] = super(Singleton, self).__call__(*args, **kwargs)
         return self._instance[self]
 
 
